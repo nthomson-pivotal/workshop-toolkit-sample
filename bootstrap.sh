2385deb4a28608c287ea88d64cb9c07d57f0274e
Niall Thomson <nthomson@pivotal.io>
2019-07-18 10:23:00 -0700

    Testing cf login

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,16 @@
 
 set -e
 
-env
+BIN_DIR=$CODER_DIR/bin
 
-kubectl get pods
+mkdir -p $BIN_DIR
+
+if [ ! -f "$BIN_DIR/cf" ]; then
+  wget -O /tmp/cf.tgz https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.46.0/cf-cli_6.46.0_linux_x86-64.tgz
+
+  tar -zxf /tmp/cf.tgz cf -C $BIN_DIR
+
+  chmod +x $BIN_DIR/cf
+fi
+
+$BIN_DIR/cf login -a $CF_API -u admin -p $CF_PASSWORD
