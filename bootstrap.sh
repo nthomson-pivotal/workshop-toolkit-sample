3ddf4188d2d68de1e405236e0087cd473c30716f
Niall Thomson <nthomson@pivotal.io>
2019-07-18 12:13:38 -0700

    Testing cf login

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,9 @@ mkdir -p $BIN_DIR
 if [ ! -f "$BIN_DIR/cf" ]; then
   wget -O /tmp/cf.tgz https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.46.0/cf-cli_6.46.0_linux_x86-64.tgz
 
-  tar -zxf /tmp/cf.tgz cf -C $BIN_DIR
+  tar -zxf /tmp/cf.tgz cf
+
+  mv cf $BIN_DIR/cf
 
   chmod +x $BIN_DIR/cf
 fi
