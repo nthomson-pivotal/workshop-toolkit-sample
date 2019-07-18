995308ccafa875e02a1ce148e57357767ad23c22
Niall Thomson <nthomson@pivotal.io>
2019-07-18 12:25:34 -0700

    Testing cf login

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,3 +17,23 @@ if [ ! -f "$BIN_DIR/cf" ]; then
 fi
 
 $BIN_DIR/cf login -a $CF_API -u admin -p $CF_PASSWORD --skip-ssl-validation
+
+$BIN_DIR/cf create-space -o workspaces $ID
+
+password='pa55w0rd'
+
+cf create-user $ID $password
+cf set-space-role $ID workspaces $ID SpaceDeveloper
+
+secret_password=$(echo $password | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $ID
+  namespace: workspaces
+type: Opaque
+data:
+  cf_password: $secret_password
+EOF
