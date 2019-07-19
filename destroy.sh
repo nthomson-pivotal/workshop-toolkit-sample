5df898c310b292cddb200473d87731622a58a518
Niall Thomson <nthomson@pivotal.io>
2019-07-18 22:07:36 -0700

    Testing destroy script

---
+++ destroy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+BIN_DIR=$CODER_DIR/bin
+
+source /mnt/coder/config
+
+$BIN_DIR/cf login -a $CF_API -u admin -p $CF_PASSWORD --skip-ssl-validation -o system
+
+$BIN_DIR/cf delete-space -o workspaces $WORKSHOP_ID -f
+
+$BIN_DIR/cf deletes-user $WORKSHOP_ID -f
