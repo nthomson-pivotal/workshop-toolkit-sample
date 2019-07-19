02ee7966963e62b6ac4e8bfd3045eb644f6f7cef
Niall Thomson <nthomson@pivotal.io>
2019-07-18 17:04:46 -0700

    Testing cf login

--- init.sh
+++ init.sh
@@ -13,7 +13,10 @@ git clone https://github.com/cloudfoundry-samples/spring-music.git
 # Link CF executable
 sudo ln -s /mnt/coder/bin/cf /usr/bin/cf
 
-echo 'source /mnt/coder/config' >> ~/.bashrc
+echo 'source /mnt/coder/config' > ~/.bashrc/cf.bashrc
+chmod +x ~/.bashrc/cf.bashrc
+
+source /mnt/coder/config
 
 # Log in to CF CLI
-#cf login -a $cf_api -u admin -p $cf_password --skip-ssl-validation
+cf login -a $CF_API -u $WORKSHOP_ID -p $CF_PASSWORD --skip-ssl-validation
