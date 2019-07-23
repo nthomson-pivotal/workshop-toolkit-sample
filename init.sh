c63d1ac8c5a7d6fc1c587b59cc999f0ea499a93a
Niall Thomson <nthomson@pivotal.io>
2019-07-23 12:35:02 -0700

    Moved JDK install to bootstrap

--- init.sh
+++ init.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-sudo apt-get update -y
-
-# Packages
-sudo apt-get install -y openjdk-8-jdk git
-
 # Fetch spring-music
 git clone https://github.com/cloudfoundry-samples/spring-music.git
 
