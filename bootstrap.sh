ab5d9c5ac2bf677bbd145926e6fead62a17a1a61
Niall Thomson <nthomson@pivotal.io>
2019-07-23 11:36:58 -0700

    Update bootstrap.sh
    Making bin directory now in server image

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,6 @@ set -e
 
 BIN_DIR=$CODER_DIR/bin
 
-mkdir -p $BIN_DIR
-
 if [ ! -f "$BIN_DIR/cf" ]; then
   wget -O /tmp/cf.tgz https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.46.0/cf-cli_6.46.0_linux_x86-64.tgz
 
