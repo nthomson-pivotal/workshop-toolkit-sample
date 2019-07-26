5ff93d7d1a93ad20a5e3fe406423b20e628b1aa7
Niall Thomson <nthomson@pivotal.io>
2019-07-26 07:40:19 -0700

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -4,8 +4,10 @@ set -e
 
 BIN_DIR=$CODER_DIR/bin
 
-$BIN_DIR/cf login -a $CF_API -u admin -p $CF_PASSWORD --skip-ssl-validation -o system
+if [ ! -z "$CF_API" ]; then
+  $BIN_DIR/cf login -a $CF_API -u admin -p $CF_PASSWORD --skip-ssl-validation -o system
 
-$BIN_DIR/cf delete-space -o workspaces $WORKSHOP_ID -f
+  $BIN_DIR/cf delete-space -o workspaces $WORKSHOP_ID -f
 
-$BIN_DIR/cf delete-user $WORKSHOP_ID -f
+  $BIN_DIR/cf delete-user $WORKSHOP_ID -f
+fi
