7a1e49a4c04a3670c5e41c704c10ca7e2874dcb0
Niall Thomson <nthomson@pivotal.io>
2019-07-26 00:06:32 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,11 @@ set -e
 # Fetch spring-music
 git clone https://github.com/cloudfoundry-samples/spring-music.git
 
-source /mnt/coder/bashrc.d/cf.bashrc
+if [ -f /mnt/coder/bashrc.d/cf.bashrc ]; then
+  source /mnt/coder/bashrc.d/cf.bashrc
+fi
 
-# Log in to CF CLI
-/mnt/coder/bin/cf login -a $CF_API -u $WORKSHOP_ID -p $CF_PASSWORD --skip-ssl-validation
+if [ ! -z "$CF_API" ]; then
+  # Log in to CF CLI
+  /mnt/coder/bin/cf login -a $CF_API -u $WORKSHOP_ID -p $CF_PASSWORD --skip-ssl-validation
+fi
