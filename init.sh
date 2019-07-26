9db1a3c7e2128835e6ec4183bc22ce2a55f53b51
Niall Thomson <nthomson@pivotal.io>
2019-07-26 09:54:21 -0700

    Tidy up

--- init.sh
+++ init.sh
@@ -5,11 +5,7 @@ set -e
 # Fetch spring-petclinic
 git clone https://github.com/spring-projects/spring-petclinic.git
 
-if [ -f /mnt/coder/bashrc.d/cf.bashrc ]; then
-  source /mnt/coder/bashrc.d/cf.bashrc
-fi
-
 if [ ! -z "$CF_API" ]; then
   # Log in to CF CLI
-  /mnt/coder/bin/cf login -a $CF_API -u $WORKSHOP_ID -p $CF_PASSWORD --skip-ssl-validation
+  cf login -a $CF_API -u $WORKSHOP_ID -p $CF_PASSWORD --skip-ssl-validation
 fi
