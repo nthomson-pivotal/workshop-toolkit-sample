3be8c4b127e8cbb3bc56c3a1c9fceb564bd342b2
Niall Thomson <nthomson@pivotal.io>
2019-07-27 17:39:10 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,15 +10,14 @@ if [ ! -z "$CF_API" ]; then
   cf login -a $CF_API -u $WORKSHOP_ID -p $CF_PASSWORD --skip-ssl-validation
 fi
 
-mkdir -p ${EXTENSIONS_DIR}/java \
-    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/redhat/vsextensions/java/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java extension
+#mkdir -p ${EXTENSIONS_DIR}/java \
+#    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/redhat/vsextensions/java/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java extension
     
-mkdir -p ${EXTENSIONS_DIR}/java-debugger \
-    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-java-debug/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java-debugger extension
+#mkdir -p ${EXTENSIONS_DIR}/java-debugger \
+#    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-java-debug/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java-debugger extension
 
-mkdir -p ${EXTENSIONS_DIR}/java-test \
-    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-java-test/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java-test extension
-
+#mkdir -p ${EXTENSIONS_DIR}/java-test \
+#    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-java-test/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java-test extension
     
-mkdir -p ${EXTENSIONS_DIR}/maven \
-    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-maven/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/maven extension
+#mkdir -p ${EXTENSIONS_DIR}/maven \
+#    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-maven/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/maven extension
