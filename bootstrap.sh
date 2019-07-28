e52a1ef7d32998310dd197f6030bd3c8954c366c
Niall Thomson <nthomson@pivotal.io>
2019-07-27 17:25:32 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,14 @@ set -e
 
 BIN_DIR=$CODER_DIR/bin
 
-#wget -O jdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u222b10.tar.gz
+wget -O jdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u222-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u222b10.tar.gz
 
-#tar -xf jdk.tar.gz -C $CODER_DIR
+tar -xf jdk.tar.gz -C $CODER_DIR
 
-#cat << EOF > /mnt/coder/bashrc.d/jdk.bashrc
-#export PATH=\$PATH:/mnt/coder/jdk8u222-b10/bin
-#EOF
-#chmod +x /mnt/coder/bashrc.d/jdk.bashrc
+cat << EOF > /mnt/coder/bashrc.d/jdk.bashrc
+export PATH=\$PATH:/mnt/coder/jdk8u222-b10/bin
+EOF
+chmod +x /mnt/coder/bashrc.d/jdk.bashrc
 
 if [ ! -f "$BIN_DIR/cf" ]; then
   wget -O /tmp/cf.tgz https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.46.0/cf-cli_6.46.0_linux_x86-64.tgz
