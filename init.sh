e24e3ba4428ca69fcf4f549994f4d4bb8e5901a1
Niall Thomson <nthomson@pivotal.io>
2019-11-12 12:38:47 -0800

    Extensions now installed in base image

--- init.sh
+++ init.sh
@@ -9,16 +9,3 @@ if [ ! -z "$CF_API" ]; then
   # Log in to CF CLI
   cf login -a $CF_API -u $WORKSHOP_ID -p $CF_PASSWORD --skip-ssl-validation
 fi
-
-# Setup Java Extension
-mkdir -p ${EXTENSIONS_DIR}/java \
-    && curl -JLs https://marketplace.visualstudio.com/_apis/public/gallery/publishers/redhat/vsextensions/java/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java extension
-
-mkdir -p ${EXTENSIONS_DIR}/java-debugger \
-    && curl -JLs https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-java-debug/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java-debugger extension
-
-mkdir -p ${EXTENSIONS_DIR}/java-test \
-    && curl -JLs https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-java-test/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/java-test extension
-
-mkdir -p ${EXTENSIONS_DIR}/maven \
-    && curl -JLs https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscjava/vsextensions/vscode-maven/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/maven extension
