01593262556fc6e5eeb85a1dc3065862ac80f41c
Niall Thomson <nthomson@pivotal.io>
2020-03-19 18:20:33 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,14 @@ set -e
 # Create empty repos directory
 mkdir ~/project/repos
 
+# OS packages
+RUN apt-get update && apt-get install --no-install-recommends -y \
+    default-jre-headless \
+    default-jdk-headless \
+    maven \
+    gradle
+
+# Extensions
 code-server --install-extension redhat.java
 code-server --install-extension vscjava.vscode-spring-initializr
 code-server --install-extension vscjava.vscode-maven
