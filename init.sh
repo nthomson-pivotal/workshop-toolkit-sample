25f4ed68b76ed26538c9979112591570e2b6c2d5
Niall Thomson <nthomson@pivotal.io>
2020-03-19 18:04:00 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,6 +9,8 @@ code-server --install-extension redhat.java
 code-server --install-extension vscjava.vscode-spring-initializr
 code-server --install-extension vscjava.vscode-maven
 code-server --install-extension vscjava.vscode-java-test
+code-server --install-extension ecmel.vscode-spring-boot
+code-server --install-extension vscjava.vscode-spring-boot-dashboard
 
 if [ ! -z "$CF_API" ]; then
   # Log in to CF CLI
